a848e7f4a865ed41a49a35db8b6a3b05ed125462
Ryan Gordon <ryangordon@duck.com>
2023-12-05 02:49:30 +1300

    Day 3 Part 2 complete

--- Day 3/gear_ratios.py
+++ Day 3/gear_ratios.py
@@ -4,20 +4,81 @@
 # Author: Ryan Gordon
 # Response for advent of code day 3 2023
 
-NON_SYMBOLS = tuple("@#$%&*-=+/")
+from functools import reduce
 
-all_lines = [None for i in range(sum(1 for line in open("engine.txt", "r")))]
+def find_adjacent_part_numbers(row: int, col: int) -> list:
+    """
+    Finds all part numbers adjacent to a given entry and returns as a list.
+
+    row -- the row of the entry.
+    col -- the column of the entry.
+    """
+    part_numbers = []
+    indices_covered = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+
+            if [i, j] in indices_covered:
+                continue
+
+            try:
+                int(all_lines[row + i][col + j])
+
+                start_index = col + j
+                while True:
+                    try:
+                        int(all_lines[row + i][start_index])
+                    except (ValueError, IndexError):
+                        start_index += 1
+                        break
+
+                    start_index -= 1
+
+                number, offset = "", start_index - (col + j)
+                
+                while True:
+                    character = all_lines[row + i][start_index]
+                    try:
+                        int(character)
+                        number += character
+                        indices_covered.append([i, j + offset])
+                    except (ValueError, IndexError):
+                        break
+
+                    start_index += 1
+                    offset += 1
+
+                part_numbers.append(int(number))
+            except ValueError:
+                pass
+    return part_numbers
+
+SYMBOLS = tuple("@#$%&*-=+/")
+
+global all_lines
+all_lines = []
 with open("engine.txt", "r") as reader:
-    for i, line in enumerate(reader.readlines()):
-        all_lines[i] = list(line)
+    for line in reader.readlines():
+        all_lines.append(list(line))
 
 if __name__ == "__main__":
-    total = 0
+    part_numbers_sum = 0
+    gear_ratios_sum = 0
     section = ""
     for i, line in enumerate(all_lines):
         for j, char in enumerate(line):
             character = all_lines[i][j]
-            
+
+            # Part 2
+            if character == "*":
+                # Search for the presence of two adjacent part numbers.
+                part_numbers = find_adjacent_part_numbers(i, j)
+                if len(part_numbers) == 2:
+                    gear_ratios_sum += reduce(lambda x, y: x*y, part_numbers)
+
+            # Part 1            
             try:
                 int(character)
                 section += character
@@ -31,12 +92,12 @@ if __name__ == "__main__":
                 # Add number if applicable.
                 symbol_present = False
                 
-                if character in NON_SYMBOLS:
+                if character in SYMBOLS:
                     symbol_present = True
 
                 # Search to left and right of number.
                 try:
-                    if all_lines[i][j - len(section) - 1] in NON_SYMBOLS:
+                    if all_lines[i][j - len(section) - 1] in SYMBOLS:
                         symbol_present = True
                 except IndexError:
                     pass
@@ -44,13 +105,13 @@ if __name__ == "__main__":
                 # Search above and below number.
                 for k in range((len(section) + 1) * -1, 1):
                     try:
-                        if all_lines[i - 1][j + k] in NON_SYMBOLS:
+                        if all_lines[i - 1][j + k] in SYMBOLS:
                             symbol_present = True
                     except IndexError:
                         pass
 
                     try:
-                        if all_lines[i + 1][j + k] in NON_SYMBOLS:
+                        if all_lines[i + 1][j + k] in SYMBOLS:
                             symbol_present = True
                     except IndexError:
                         pass
@@ -59,7 +120,8 @@ if __name__ == "__main__":
                         break
 
                 if symbol_present:
-                    total += int(section)
+                    part_numbers_sum += int(section)
 
                 section = ""
-    print(total)
+    print(f"Part 1: {part_numbers_sum}")
+    print(f"Part 2: {gear_ratios_sum}")
