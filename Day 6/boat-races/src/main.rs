5d9a4fe8bc19d7c590d164bc20205dd2b2a55946
Ryan Gordon <ryangordon@duck.com>
2023-12-06 23:17:50 +1300

    Day 6 Part 2 complete

--- Day 6/boat-races/src/main.rs
+++ Day 6/boat-races/src/main.rs
@@ -2,18 +2,38 @@ use std::fs;
 use std::collections::HashMap;
 
 fn main() {
+    let part_two_time: u64;
+    let part_two_record: u64;
     let mut times_and_records: HashMap<u16, u16> = HashMap::new();
 
     {
         let file = fs::read_to_string("src/races.txt").unwrap();
         let lines: Vec<&str> = file.split_terminator("\n").collect();
 
-        let times: Vec<&str> = lines[0].split(":").collect::<Vec<&str>>()[1].split(" ").filter(|&t| !t.is_empty()).collect();
-        let distances: Vec<&str> = lines[1].split(":").collect::<Vec<&str>>()[1].split(" ").filter(|&d| !d.is_empty()).collect();
+        let raw_time_data: &str = lines[0].split(":").collect::<Vec<&str>>()[1];
+        let raw_record_data: &str = lines[1].split(":").collect::<Vec<&str>>()[1];
+
+        let times: Vec<&str> = raw_time_data.split(" ").filter(|&t| !t.is_empty()).collect();
+        let distances: Vec<&str> = raw_record_data.split(" ").filter(|&d| !d.is_empty()).collect();
         
         for i in 0..times.len() {
             times_and_records.insert(times[i].parse::<u16>().unwrap(), distances[i].parse::<u16>().unwrap());
         }
+
+        println!("{}", raw_record_data);
+
+        part_two_time = raw_time_data.split(" ")
+            .filter(|&ts| !ts.is_empty())
+            .collect::<Vec<&str>>()
+            .join("")
+            .parse::<u64>()
+            .unwrap();
+        part_two_record = raw_record_data.split(" ")
+            .filter(|&rs| !rs.is_empty())
+            .collect::<Vec<&str>>()
+            .join("")
+            .parse::<u64>()
+            .unwrap();
     }
 
     let mut margin_of_error: u32 = 0;
@@ -33,6 +53,15 @@ fn main() {
         }
     }
 
-    println!("{}", margin_of_error);
+    let mut ways_of_winning: u32 = 0;
+    for hold in 0..(part_two_time + 1) {
+        let distance = hold * (part_two_time - hold);
+        if distance > part_two_record {
+            ways_of_winning += 1;
+        }
+    }
+
+    println!("Part 1: {}", margin_of_error);
+    println!("Part 2: {}", ways_of_winning);
 }
 
