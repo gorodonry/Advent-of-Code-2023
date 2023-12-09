212c5647bccec1d9e0322145c980ac95f88fd174
Ryan Gordon <ryangordon@duck.com>
2023-12-09 23:46:14 +1300

    Day 9 Part 2 complete

--- Day 9/mirage-maintenance/src/main.rs
+++ Day 9/mirage-maintenance/src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     let file = fs::read_to_string("src/readings.txt").unwrap();
     let lines: Vec<&str> = file.split_terminator("\n").collect();
 
-    let mut total: i64 = 0;
+    let mut part_one_total: i64 = 0;
+    let mut part_two_total: i64 = 0;
     for line in lines.into_iter() {
         let mut layers: Vec<Vec<i32>> = vec![line
             .split(" ")
@@ -31,20 +32,31 @@ fn main() {
 
         {
             let mut last_layer = layers.remove(layers.len() - 1);
-            last_layer.push(0);
+            last_layer.extend([0, 0]);
             layers.push(last_layer);
         }
 
+        // Part 1.
         for i in (0..(layers.len() - 1)).rev() {
             let mut layer = layers.remove(i);
             layer.push(layer[layer.len() - 1] + layers[i][layer.len() - 1]);
             layers.insert(i, layer);
         }
 
-        total += layers[0][layers[0].len() - 1] as i64;
+        part_one_total += layers[0][layers[0].len() - 1] as i64;
+
+        // Part 2.
+        for i in (0..(layers.len() - 1)).rev() {
+            let mut layer = layers.remove(i);
+            layer.insert(0, layer[0] - layers[i][0]);
+            layers.insert(i, layer);
+        }
+
+        part_two_total += layers[0][0] as i64;
     }
 
-    println!("{}", total);
+    println!("Part 1: {}", part_one_total);
+    println!("Part 2: {}", part_two_total);
 }
 
 fn all_zeroes(ints: &Vec<isize>) -> bool {
