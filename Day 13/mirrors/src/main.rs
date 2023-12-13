43dd6a15e8bec35309a2db55e24badeed26dd3e4
Ryan Gordon <ryangordon@duck.com>
2023-12-14 04:22:40 +1300

    Day 13 Part 2 complete

--- Day 13/mirrors/src/main.rs
+++ Day 13/mirrors/src/main.rs
@@ -5,11 +5,14 @@ fn main() {
     let lines: Vec<&str> = file.split_terminator("\n").collect();
 
     let mut current_pattern: Vec<&str> = Vec::new();
-    let mut total: u32 = 0;
+    let mut part_one_total: u32 = 0;
+    let mut part_two_total: u32 = 0;
 
     for line in lines.into_iter() {
         if line.is_empty() {
-            total += get_pattern_summary(&current_pattern);
+            let part_one_summary = get_pattern_summary(&current_pattern, &Vec::new());
+            part_one_total += part_one_summary.value;
+            part_two_total += get_smudge_summary(&current_pattern, &part_one_summary);
 
             current_pattern.clear();
         } else {
@@ -18,19 +21,57 @@ fn main() {
     }
 
     if !current_pattern.is_empty() {
-        total += get_pattern_summary(&current_pattern);
+        let part_one_summary = get_pattern_summary(&current_pattern, &Vec::new());
+        part_one_total += part_one_summary.value;
+        part_two_total += get_smudge_summary(&current_pattern, &part_one_summary);
     }
 
-    println!("{}", total);
+    println!("Part 1: {}", part_one_total);
+    println!("Part 2: {}", part_two_total);
 }
 
-fn get_pattern_summary(pattern: &Vec<&str>) -> u32 {
+fn get_smudge_summary(current_pattern: &Vec<&str>, original_summary: &Summary) -> u32 {
+    let mut discount_summaries: Vec<Summary> = Vec::from([original_summary.clone()]);
+    let mut smudge_summary: u32 = 0;
+
+    for i in 0..current_pattern.len() {
+        for j in 0..current_pattern[0].len() {
+            let mut mutant = current_pattern.clone();
+            let mut row: Vec<char> = current_pattern[i].chars().collect();
+
+            match row[j] {
+                '.' => row[j] = '#',
+                '#' => row[j] = '.',
+                _ => panic!("This shouldn't be happening........")
+            }
+
+            let new_row = row.iter().collect::<String>();
+
+            mutant[i] = new_row.as_str();
+
+            let summary = get_pattern_summary(&mutant, &discount_summaries);
+
+            smudge_summary += summary.value;
+            discount_summaries.push(summary);
+        }
+    }
+
+    smudge_summary
+}
+
+fn get_pattern_summary(pattern: &Vec<&str>, discount_summaries: &Vec<Summary>) -> Summary {
     let mut summary: u32 = 0;
 
     let mut row_match_indices: Vec<usize> = Vec::new();
 
-    for i in 0..(pattern.len() - 1) {
+    'row_loop: for i in 0..(pattern.len() - 1) {
         if pattern[i] == pattern[i + 1] {
+            for discount_summary in discount_summaries.iter() {
+                if discount_summary.row_match_indices.contains(&i) {
+                    continue 'row_loop;
+                }
+            }
+
             let row_match_index = i;
 
             if row_match_index == 0 {
@@ -56,7 +97,7 @@ fn get_pattern_summary(pattern: &Vec<&str>) -> u32 {
 
     let mut column_match_indices: Vec<usize> = Vec::new();
 
-    for i in 0..(pattern[0].len() - 1) {
+    'column_loop: for i in 0..(pattern[0].len() - 1) {
         if pattern
             .iter()
             .map(|&row| row.chars().collect::<Vec<char>>()[i])
@@ -66,6 +107,12 @@ fn get_pattern_summary(pattern: &Vec<&str>) -> u32 {
                 .map(|&row| row.chars().collect::<Vec<char>>()[i + 1])
                 .collect::<String>()
         {
+            for discount_summary in discount_summaries.iter() {
+                if discount_summary.column_match_indices.contains(&i) {
+                    continue 'column_loop;
+                }
+            }
+
             let column_match_index = i;
 
             if column_match_index == 0 {
@@ -97,13 +144,28 @@ fn get_pattern_summary(pattern: &Vec<&str>) -> u32 {
         }
     }
 
-    for index in row_match_indices.into_iter() {
+    for index in row_match_indices.iter() {
         summary += 100 * (index + 1) as u32;
     }
 
-    for index in column_match_indices.into_iter() {
-        summary += index as u32 + 1;
+    for index in column_match_indices.iter() {
+        summary += *index as u32 + 1;
     }
 
-    summary
+    Summary { value: summary, row_match_indices, column_match_indices }
 }
+
+#[derive(Debug, Clone)]
+struct Summary {
+    value: u32,
+    row_match_indices: Vec<usize>,
+    column_match_indices: Vec<usize>
+}
+
+impl PartialEq for Summary {
+    fn eq(&self, other: &Self) -> bool {
+        (&self.value, &self.row_match_indices[..], &self.column_match_indices[..]) == (&other.value, &other.row_match_indices[..], &other.column_match_indices[..])
+    }
+}
+
+impl Eq for Summary {}
