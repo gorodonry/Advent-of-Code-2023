5198f39c87f5c281c0137325b126e733b5a5f96e
Ryan Gordon <ryangordon@duck.com>
2023-12-19 02:12:14 +1300

    YESSSSSSS IT WORKSSSSSSgit add .! (Day 14 Part 2 complete)

--- Day 14/rocks/src/main.rs
+++ Day 14/rocks/src/main.rs
@@ -1,18 +1,18 @@
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("src/test.txt").unwrap();
+    let file = fs::read_to_string("src/platform.txt").unwrap();
     let mut platform: Vec<Vec<char>> = file.split_terminator("\n").collect::<Vec<&str>>().iter().map(|&row| row.chars().collect::<Vec<char>>()).collect();
 
-    let mut columns: Vec<Vec<char>> = Vec::new();
+    let mut part_one_platform: Vec<Vec<char>> = Vec::new();
 
     for i in 0..platform[0].len() {
-        columns.push(platform.iter().map(|row| row[i].clone()).collect::<Vec<char>>());
+        part_one_platform.push(platform.iter().map(|row| row[i].clone()).collect::<Vec<char>>());
     }
 
     let mut part_one_load: u32 = 0;
 
-    for mut column in columns.into_iter() {
+    for mut column in part_one_platform.into_iter() {
         while !all_round_rocks_north(&column) {
             roll_rocks(&mut column);
         }
@@ -24,9 +24,34 @@ fn main() {
         }
     }
 
-    for k in 0..1000000000 {
-        println!("{}", k);
-        
+    let report = spin_cycle(&mut platform, 1000000000, true);
+    spin_cycle(&mut platform, (1000000000 - report.cycles_processed) % report.repetition_cycle, false);
+
+    print_platform(&platform);
+
+    let mut part_two_load: u32 = 0;
+    
+    for col in 0..platform[0].len() {
+        let column = platform.iter().map(|row| row[col].clone()).collect::<Vec<char>>();
+
+        for i in 0..column.len() {
+            if column[i] == 'O' {
+                part_two_load += (column.len() - i) as u32;
+            }
+        }
+    }
+
+    println!("Part 1: {}", part_one_load);
+    println!("Part 2: {}", part_two_load);
+}
+
+fn spin_cycle(platform: &mut Vec<Vec<char>>, cycles: usize, break_after_repitition: bool) -> SpinCycleReport {
+    let mut cache: Vec<Vec<Vec<char>>> = Vec::new();
+    let mut repetition_begins: usize = 0;
+    let mut repetition_cycle: usize = 0;
+    let mut cycles_processed: usize = 0;
+
+    for i in 0..cycles {
         // Roll north.
         for i in 0..platform[0].len() {
             let mut column: Vec<char> = platform.iter().map(|row| row[i].clone()).collect();
@@ -74,19 +99,24 @@ fn main() {
 
             platform[i] = row.iter().rev().map(|c| *c).collect::<Vec<char>>();
         }
-    }
 
-    let mut part_two_load: u32 = 0;
-    for vector in platform.iter() {
-        for i in 0..vector.len() {
-            if vector[i] == 'O' {
-                part_two_load += (vector.len() - i) as u32;
+        cycles_processed += 1;
+
+        if repetition_begins == 0 && cache.contains(&platform) {
+            cache.clear();
+            repetition_begins = i;
+        } else if repetition_cycle == 0 && cache.contains(&platform) {
+            repetition_cycle = i - repetition_begins;
+
+            if break_after_repitition {
+                break;
             }
         }
+
+        cache.push(platform.clone());
     }
 
-    println!("Part 1: {}", part_one_load);
-    println!("Part 2: {}", part_two_load);
+    SpinCycleReport { cycles_processed, repetition_cycle }
 }
 
 fn roll_rocks(vector: &mut Vec<char>) {
@@ -106,4 +136,18 @@ fn all_round_rocks_north(vector: &Vec<char>) -> bool {
     }
 
     true
+}
+
+fn print_platform(platform: &Vec<Vec<char>>) {
+    for line in platform.iter() {
+        println!("{}", line.iter().collect::<String>());
+    }
+
+    println!("----------");
+}
+
+#[derive(Debug)]
+struct SpinCycleReport {
+    cycles_processed: usize,
+    repetition_cycle: usize
 }
