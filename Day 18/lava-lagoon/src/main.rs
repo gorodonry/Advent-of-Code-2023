0babca2683174f5795615ac6542236f13bff4cc2
Ryan Gordon <ryangordon@duck.com>
2023-12-19 00:29:54 +1300

    Forgot to format code

--- Day 18/lava-lagoon/src/main.rs
+++ Day 18/lava-lagoon/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
 
 fn main() {
     let file = fs::read_to_string("src/instructions.txt").unwrap();
@@ -13,7 +13,9 @@ fn main() {
     // Determine outline.
     for line in instructions.into_iter() {
         let direction = line.split(" ").collect::<Vec<&str>>()[0];
-        let length = line.split(" ").collect::<Vec<&str>>()[1].parse::<u8>().unwrap();
+        let length = line.split(" ").collect::<Vec<&str>>()[1]
+            .parse::<u8>()
+            .unwrap();
 
         match direction {
             "U" => {
@@ -27,7 +29,7 @@ fn main() {
                 }
 
                 current_location.row -= length as usize;
-            },
+            }
             "D" => {
                 let num_rows = map.len();
 
@@ -43,7 +45,7 @@ fn main() {
                 }
 
                 current_location.row += length as usize;
-            },
+            }
             "L" => {
                 for col in 1..=length {
                     if (current_location.col as isize - col as isize) < 0 {
@@ -55,13 +57,13 @@ fn main() {
                 }
 
                 current_location.col -= length as usize;
-            },
+            }
             "R" => {
                 let num_cols: usize;
 
                 match map.get(0) {
                     Some(val) => num_cols = val.len(),
-                    None => num_cols = 0
+                    None => num_cols = 0,
                 }
 
                 let mut num_cols_inserted: u8 = 0;
@@ -76,8 +78,8 @@ fn main() {
                 }
 
                 current_location.col += length as usize;
-            },
-            _ => panic!("Well that's... interesting")
+            }
+            _ => panic!("Well that's... interesting"),
         }
 
         number_of_edges += length as u16;
@@ -119,7 +121,11 @@ fn main() {
                         continue;
                     }
 
-                    if neighbour.row == 0 || neighbour.col == 0 || neighbour.row == map.len() - 1 || neighbour.col == map[0].len() - 1 {
+                    if neighbour.row == 0
+                        || neighbour.col == 0
+                        || neighbour.row == map.len() - 1
+                        || neighbour.col == map[0].len() - 1
+                    {
                         external = true;
                     }
 
@@ -151,16 +157,28 @@ fn get_adjacent_locations(location: &Location) -> Vec<Location> {
     let mut adjacent_locations: Vec<Location> = Vec::new();
 
     if location.row > 0 {
-        adjacent_locations.push(Location { row: location.row - 1, col: location.col });
+        adjacent_locations.push(Location {
+            row: location.row - 1,
+            col: location.col,
+        });
     }
 
-    adjacent_locations.push(Location { row: location.row + 1, col: location.col });
+    adjacent_locations.push(Location {
+        row: location.row + 1,
+        col: location.col,
+    });
 
     if location.col > 0 {
-        adjacent_locations.push(Location { row: location.row, col: location.col - 1 });
+        adjacent_locations.push(Location {
+            row: location.row,
+            col: location.col - 1,
+        });
     }
 
-    adjacent_locations.push(Location { row: location.row, col: location.col + 1 });
+    adjacent_locations.push(Location {
+        row: location.row,
+        col: location.col + 1,
+    });
 
     adjacent_locations
 }
@@ -170,7 +188,7 @@ fn insert_rows(map: &mut Vec<Vec<char>>, number_of_rows: usize, below_index: usi
 
     match map.get(0) {
         Some(val) => cols = val.len(),
-        None => cols = 0
+        None => cols = 0,
     }
 
     for _ in 0..number_of_rows {
@@ -189,5 +207,5 @@ fn insert_cols(map: &mut Vec<Vec<char>>, number_of_cols: usize, below_index: usi
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Location {
     row: usize,
-    col: usize
+    col: usize,
 }
