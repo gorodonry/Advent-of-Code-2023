9db1ac471afcbefcd2291b4c632edaf723cc1c3f
Ryan Gordon <ryangordon@duck.com>
2023-12-19 15:11:03 +1300

    Refactor part 1 response

--- Day 12/hot-springs/src/main.rs
+++ Day 12/hot-springs/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
@@ -14,61 +15,81 @@ fn main() {
             .iter()
             .map(|&e| e.parse::<u8>().unwrap())
             .collect();
-        let mut valid_arrangements: u16 = 0;
 
-        for arrangement in get_arrangements(&springs).iter() {
-            let mut broken_amounts_found: Vec<u8> = Vec::new();
-            let mut current_broken_sequence_length: u8 = 0;
-
-            for c in arrangement.iter() {
-                if *c == '#' {
-                    current_broken_sequence_length += 1;
-                } else if current_broken_sequence_length != 0 {
-                    broken_amounts_found.push(current_broken_sequence_length);
-                    current_broken_sequence_length = 0;
-                }
-            }
-
-            if current_broken_sequence_length != 0 {
-                broken_amounts_found.push(current_broken_sequence_length);
-            }
-
-            if broken_amounts[..] == broken_amounts_found[..] {
-                valid_arrangements += 1;
-            }
-        }
-
-        total += valid_arrangements as u32;
+        total += count_arrangements(&springs, &broken_amounts, true);
     }
 
     println!("{}", total);
 }
 
-fn get_arrangements(springs: &Vec<char>) -> Vec<Vec<char>> {
-    let mut arrangements: Vec<Vec<char>> = Vec::new();
-    let mut unknown_index: i16 = -1;
+fn count_arrangements(
+    springs: &Vec<char>,
+    broken_sections: &Vec<u8>,
+    next_spring_can_be_broken: bool,
+) -> u32 {
+    let mut result: u32 = 0;
 
-    for i in 0..springs.len() {
-        if springs[i] == '?' {
-            unknown_index = i as i16;
-            break;
+    if springs.len() == 0 {
+        if broken_sections.len() == 0 {
+            return 1;
+        } else {
+            return 0;
         }
     }
 
-    if unknown_index == -1 {
-        arrangements.push(springs.clone());
-    } else {
-        let mut prefix: Vec<char> = springs[..=unknown_index as usize].to_vec();
-        for spring_type in vec!['.', '#'].iter() {
-            prefix[unknown_index as usize] = *spring_type;
-            for suffix in get_arrangements(&springs[(unknown_index as usize + 1)..].to_vec()).iter()
-            {
-                let mut arrangement = prefix.clone();
-                arrangement.extend(suffix);
-                arrangements.push(arrangement);
-            }
+    if broken_sections.len() == 0 {
+        if springs.contains(&'#') {
+            return 0;
+        } else {
+            return 1;
         }
     }
 
-    arrangements
+    if broken_sections.len() == 1 && springs.len() == broken_sections[0] as usize {
+        if springs.contains(&'.') || !next_spring_can_be_broken {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // First entry is a working spring.
+    if vec!['.', '?'].contains(&springs[0]) {
+        result += count_arrangements(&springs[1..].to_vec(), broken_sections, true);
+    }
+
+    // First entry is a broken spring.
+    if next_spring_can_be_broken
+        && vec!['#', '?'].contains(&springs[0])
+        && springs.len() > broken_sections[0] as usize
+    {
+        // If a . is in the arrangement in violation of the number of consecutive broken springs, we can safely ignore it.
+        let broken_section_length = broken_sections[0] as usize;
+        if !springs[..broken_section_length].contains(&'.') {
+            result += count_arrangements(
+                &springs[broken_section_length..].to_vec(),
+                &broken_sections[1..].to_vec(),
+                false,
+            );
+        }
+    }
+
+    result
+}
+
+fn multiply_input(input: &Vec<char>, amount: usize) -> Vec<char> {
+    let mut output: Vec<char> = Vec::new();
+
+    if amount == 0 {
+        return output;
+    }
+
+    for _ in 0..(amount - 1) {
+        output.extend(input.clone());
+        output.push('?');
+    }
+
+    output.extend(input.clone());
+
+    output
 }
