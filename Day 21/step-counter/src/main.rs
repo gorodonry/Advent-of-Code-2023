7e8084055cb97380d99ec492b198dd8a8c11d643
Ryan Gordon <ryangordon@duck.com>
2023-12-21 19:30:23 +1300

    Part 2 working on test, just needs optimisation

--- Day 21/step-counter/src/main.rs
+++ Day 21/step-counter/src/main.rs
@@ -2,26 +2,26 @@ use std::fs;
 use std::collections::HashSet;
 
 fn main() {
-    let file = fs::read_to_string("src/map.txt").unwrap();
+    let file = fs::read_to_string("src/test.txt").unwrap();
     let map: Vec<Vec<char>> = file.split_terminator("\n").collect::<Vec<&str>>().iter().map(|&line| line.chars().collect::<Vec<char>>()).collect();
     
-    let mut current_locations: HashSet<Location> = HashSet::new();
+    let mut current_locations: HashSet<InfiniteMapLocation> = HashSet::new();
     
     'find_start_loop: for row in 0..map.len() {
         for col in 0..map[0].len() {
             if map[row][col] == 'S' {
-                current_locations.insert(Location { row, col });
+                current_locations.insert(InfiniteMapLocation { row: row as isize, col: col as isize , num_rows: map.len(), num_cols: map[0].len() });
                 break 'find_start_loop;
             }
         }
     }
     
-    for _ in 0..64 {
-        let mut new_locations: HashSet<Location> = HashSet::new();
+    for _ in 0..500 {
+        let mut new_locations: HashSet<InfiniteMapLocation> = HashSet::new();
         
         for location in current_locations.iter() {
-            for adj_location in get_adjacent_locations(location, map.len(), map[0].len()).into_iter() {
-                if map[adj_location.row][adj_location.col] != '#' {
+            for adj_location in get_adjacent_locations(location).into_iter() {
+                if map[adj_location.get_row()][adj_location.get_col()] != '#' {
                     new_locations.insert(adj_location);
                 }
             }
@@ -34,30 +34,39 @@ fn main() {
     println!("{}", current_locations.len());
 }
 
-fn get_adjacent_locations(location: &Location, num_rows: usize, num_cols: usize) -> Vec<Location> {
-    let mut adjacent_locations: Vec<Location> = Vec::new();
+fn get_adjacent_locations(location: &InfiniteMapLocation) -> Vec<InfiniteMapLocation> {
+    let mut adjacent_locations: Vec<InfiniteMapLocation> = Vec::new();
     
-    if location.row > 0 {
-        adjacent_locations.push(Location { row: location.row - 1, col: location.col });
-    }
-    
-    if location.col > 0 {
-        adjacent_locations.push(Location { row: location.row, col: location.col - 1 });
-    }
-    
-    if location.row < num_rows - 1 {
-        adjacent_locations.push(Location { row: location.row + 1, col: location.col });
-    }
-    
-    if location.col < num_cols - 1 {
-        adjacent_locations.push(Location { row: location.row, col: location.col + 1 });
-    }
+    adjacent_locations.push(InfiniteMapLocation { row: location.row - 1, col: location.col, num_rows: location.num_rows, num_cols: location.num_cols });
+    adjacent_locations.push(InfiniteMapLocation { row: location.row, col: location.col - 1, num_rows: location.num_rows, num_cols: location.num_cols });
+    adjacent_locations.push(InfiniteMapLocation { row: location.row + 1, col: location.col, num_rows: location.num_rows, num_cols: location.num_cols });
+    adjacent_locations.push(InfiniteMapLocation { row: location.row, col: location.col + 1, num_rows: location.num_rows, num_cols: location.num_cols });
     
     adjacent_locations
 }
 
-#[derive(PartialEq, Eq, Hash)]
-struct Location {
-    row: usize,
-    col: usize
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct InfiniteMapLocation {
+    row: isize,
+    col: isize,
+    num_rows: usize,
+    num_cols: usize
+}
+
+impl InfiniteMapLocation {
+    fn get_row(&self) -> usize {
+        if self.row < 0 {
+            return self.num_rows - 1 - (self.num_rows as isize - (self.row + 1)) as usize % self.num_rows;
+        }
+        
+        self.row as usize % self.num_rows
+    }
+    
+    fn get_col(&self) -> usize {
+        if self.col < 0 {
+            return self.num_cols - 1 - (self.num_cols as isize - (self.col + 1)) as usize % self.num_cols;
+        }
+        
+        self.col as usize % self.num_cols
+    }
 }
